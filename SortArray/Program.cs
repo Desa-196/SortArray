51edaa31d5fbd895f192173b0efad97b477b2707
Пономарев Денис <desa-196@yandex.ru>
2024-02-18 11:12:59 +0300

    Добавьте файлы проекта.

---
+++ SortArray/Program.cs
@@ -0,0 +1,34 @@
+﻿int[,] a = { { 7, 3, 2 }, { 4, 9, 6 }, { 1, 8, 5 }};
+
+arraySort(a);
+arrayPrint(a);
+
+void arrayPrint(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.Write("\n");
+    }
+}
+
+void arraySort(int[,] array)
+{
+    int temp = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        for (int j = 0; j < array.Length - 1; j++)
+        {
+            Console.WriteLine($"{j / array.GetLength(1)}, {j % array.GetLength(1)}");
+            if (array[j / array.GetLength(1), j % array.GetLength(1)] > array[(j + 1) / array.GetLength(1), (j + 1) % array.GetLength(1)])
+            {
+                temp = array[j / array.GetLength(1), j % array.GetLength(1)];
+                array[j / array.GetLength(1), j % array.GetLength(1)] = array[(j + 1) / array.GetLength(1), (j + 1) % array.GetLength(1)];
+                array[(j + 1) / array.GetLength(1), (j + 1) % array.GetLength(1)] = temp;
+            }
+        }
+    }
+}
